3a03fedc885f0d1a53670a88ec4609b590eddf3d
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-11-24 15:56:10 -0700

    add comments

--- bash/shared/other/prompt.bash
+++ bash/shared/other/prompt.bash
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# Show a max of 4 levels of directory then truncate
 export PROMPT_DIRTRIM=4
 
+# Default to not using git, detect if we can use it below
 GIT_PS1_USEGIT=0
 
+# Two possible places for git prompt, as long as we have it in one
+# we're good.
 if [[ -e /usr/lib/git/git-core/git-sh-prompt ]]; then
   source '/usr/lib/git/git-core/git-sh-prompt'
   GIT_PS1_USEGIT=1
@@ -15,14 +19,24 @@ if [[ -e /usr/share/git/git-prompt.sh ]]; then
 fi
 
 if [[ $GIT_PS1_USEGIT -eq 1 ]]; then
+  # Show modified files with *, staged with +
   GIT_PS1_SHOWDIRTYSTATE=1
+  # Show stashed files with $
   GIT_PS1_SHOWSTASHSTATE=1
+  # Show untracked files with %
   GIT_PS1_SHOWUNTRACKEDFILES=1
+  # Compare local vs upstream.
+  # e.g. if local is ahead of upstream by one
+  # commit show u+1
   GIT_PS1_SHOWUPSTREAM="verbose git"
+  # add colours to status page. e.g. modified files are red
   GIT_PS1_SHOWCOLORHINTS=1
 fi
 
 function custom_prompt() {
+  # indicate exit status of last command
+  # either a green check for success
+  # or red x and the exit code for failure
   local last_exit=$?
   if [[ $last_exit -eq 0 ]]; then
     # shellcheck disable=SC2154
@@ -32,23 +46,27 @@ function custom_prompt() {
     local exit_status="${color_red}${i_fa_close} (\$?)"
   fi
 
+  # Indicate if we're accessing the machine via SSH
   local ssh_text=""
   if [[ -n $SSH_CLIENT ]]; then
     # shellcheck disable=SC2154
     local ssh_text="${color_cyan}(SSH) "
   fi
 
-#  # shellcheck disable=SC2154
-#  local curShell="${color_blue}("
-#  if [[ $IS_WSL == "true" ]]; then
-#    curShell+="WSL "
-#  fi
-#  if [[ "$0" == "-bash" ]] || [[ "$0" == "/bin/bash" ]]; then
-#    curShell+="bash)"
-#  else
-#    curShell+="$0)"
-#  fi
 
+  # shellcheck disable=SC2154
+  # Indicate what kind of shell we're in
+  local curShell="${color_blue}("
+  if [[ $IS_WSL == "true" ]]; then
+    curShell+="WSL "
+  fi
+  if [[ "$0" == "-bash" ]] || [[ "$0" == "/bin/bash" ]]; then
+    curShell+="bash)"
+  else
+    curShell+="$0)"
+  fi
+
+  # Actually build the prompt
   if [[ $GIT_PS1_USEGIT -eq 1 ]]; then
     local git_part
     # shellcheck disable=SC2154
