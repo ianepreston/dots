695f5fa604a26c3a81aa1b698434c268a70a7aa7
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-11-24 18:14:28 -0700

    intial commit

---
+++ bash/shared/functions/misc.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+function reload_profile() {
+  # shellcheck source=/dev/null
+  source "$HOME/.bash_profile"
+}
+
+function lsgrep() {
+  # shellcheck disable=SC2010
+  ls -A | grep -i "$@"
+}
+
+function llgrep() {
+  # shellcheck disable=SC2010
+  ls -hlA --time-style=long-iso | grep -i "$@"
+}
+
+function lsrg() {
+  # shellcheck disable=SC2012
+  ls -A | rg -S "$@"
+}
+
+function llrg() {
+  # shellcheck disable=SC2012
+  ls -hlA --time-style=long-iso | rg -S "$@"
+}
+
+function psgrep() {
+  # shellcheck disable=SC2009,SC2001
+  ps aux | grep -i "$(echo "$@" | sed "s/^\(.\)/[\1]/g")"
+}
+
+function psrg() {
+  # shellcheck disable=SC2001
+  ps aux | rg -S "$(echo "$@" | sed "s/^\(.\)/[\1]/g")"
+}
+
+function myip() {
+  curl ifconfig.co/ip
+}
+
+function usage() {
+  if [ -n "$1" ]; then
+    du -h --max-depth=1 "$1" | sort -hr
+  else
+    du -h --max-depth=1 | sort -hr
+  fi
+}
---
+++ bash/shared/functions/yt.bash
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+function yt-helper() {
+  local url
+  if [[ $IS_WSL == "1" ]]; then
+    url=$(powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command Get-Clipboard)
+  else
+    url=$(xsel -o --clipboard)
+  fi
+
+  local highFormat="bestvideo[ext=mp4][height<=?1080]+bestaudio[ext=m4a]/bestvideo[ext=webm][height<=?1080]+bestaudio[ext=webm]/best[height<=?1080]/best"
+  local lowFormat="bestvideo[ext=mp4][height<=?720]+bestaudio[ext=m4a]/bestvideo[ext=webm][height<=?720]+bestaudio[ext=webm]/best[height<=?720]/best"
+  local format="$highFormat"
+
+  if [[ "$1" == "low" ]]; then
+    format="$lowFormat"
+  fi
+
+  echo "Downloading $url"
+  youtube-dl -i -f "$format" -o "$2" "$url" "${@:3}"
+}
+
+function yt() {
+  local output
+  output="$(xdg-user-dir VIDEOS)/%(title)s-%(id)s.%(ext)s"
+  yt-helper "high" "$output" "$@"
+}
+
+function pt() {
+  local output
+  output="$(xdg-user-dir PICTURES)/!other/%(title)s-%(id)s.%(ext)s"
+  yt-helper "low" "$output" "$@"
+}
+
+# To extract an MP3 from a video, requires ffmpeg to be installed
+function ytm() {
+  local output
+  output="$(xdg-user-dir VIDEOS)/%(title)s-youtube.%(ext)s"
+
+  local url
+  if [[ $IS_WSL == "1" ]]; then
+    url=$(powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command Get-Clipboard)
+  else
+    url=$(xsel -o --clipboard)
+  fi
+
+  echo "Downloading $url"
+  youtube-dl --extract-audio --audio-format mp3 -i -o "$output" "$url" "${@:2}"
+}
